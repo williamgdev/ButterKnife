aaba22334bd56dda6d5dffc0ad8c9605b36e8e3b
androidStorm <williamg.dev@gmail.com>
2017-01-07 21:27:17 -0600

    Bind Resources

--- app/src/main/java/com/github/williamg/examples/butterknife/MainActivity.java
+++ app/src/main/java/com/github/williamg/examples/butterknife/MainActivity.java
@@ -1,16 +1,32 @@
 package com.github.williamg.examples.butterknife;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.BindColor;
+import butterknife.BindDimen;
+import butterknife.BindDrawable;
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.main_txt)
     TextView textView;
+    @BindView(R.id.main_imageview)
+    ImageView imageView;
+
+    //Bind resources
+    @BindString(R.string.hello_butterknife)
+    String string;
+    @BindDrawable(R.drawable.image)
+    Drawable image;
+    @BindColor(R.color.colorAccent)
+    int colorAccent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        textView.setText("Hello butterKnife!!!");
+        textView.setText(string);
+        textView.setTextColor(colorAccent);
+        imageView.setImageDrawable(image);
+
     }
 }
